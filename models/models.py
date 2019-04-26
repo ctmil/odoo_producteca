8411861784e336ef87efd0cb08aebec9ef7d72c7
Ignacio Buioli <ibuioli@gmail.com>
2019-04-26 19:42:29 +0000

    feat: connect producteca method

--- models/models.py
+++ models/models.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
 
 class odoo-producteca(models.Model):
 	_inherit = 'sale.order'
+
+	@api.model
+	def connect_producteca(self):
+		raise ValidationError("Connect Producteca Test")
 
 	integration_id = fields.Char('IntegrationId')
 	doc_xml = fields.Text('XML')
